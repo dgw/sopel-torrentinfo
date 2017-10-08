9e44f68c1ea764abff15e30dd227417b891e38ff
Alan Szewczyk <aszewczyk@protonmail.ch>
2017-10-08 11:01:40 +0200

    Change nyaa.si parsing to og: tags

--- README.md
+++ README.md
@@ -4,15 +4,14 @@ Sopel module that fetches info about torrent links
 ## Current status
 
 The current support is very limited, just a proof of doing something. It fetches
-only from Nyaa.si and Anidex.info, and then only a very few details.
+only from Nyaa.si and Anidex.info, and then only a very few details. More information (and possibly
+other supported sites) will come later.
 
 ## Requirements
 The torrentinfo module relies on the following Python components:
 
-* `bleach`
 * `etree` from `lxml`
 * `requests`
-* `re`
 
 ## Usage
 There are no commands. Just enable the module and it will fetch info about
--- torrentinfo.py
+++ torrentinfo.py
@@ -2,17 +2,11 @@
 torrentinfo.py - Sopel module to fetch torrent information for links sent to channels
 """
 
-# -*- coding: utf-8 -*-
-# encoding=utf8  
-import sys  
-reload(sys)  
-sys.setdefaultencoding('utf-8')
-
 from sopel import module
 import requests
-from lxml import etree
-from pyquery import PyQuery
+import lxml
 import re
+import bleach
 
 NYAA_URL = 'https://nyaa.si/view/%s'
 ANIDEX_URL = 'https://anidex.info/torrent/%s'
@@ -23,30 +17,27 @@ def nyaa_info(bot, trigger):
     try:
         r = requests.get(url=parsed_url, timeout=(10.0, 4.0))
     except requests.exceptions.ConnectTimeout:
-        returnbot.say("Connection timed out.")
+        return bot.say("[Nyaa] Connection timed out.")
     except requests.exceptions.ConnectionError:
-        return bot.say("Couldn't connect to server.")
+        return bot.say("[Nyaa] Couldn't connect to server.")
     except requests.exceptions.ReadTimeout:
-        return bot.say("Server took too long to send data.")
+        return bot.say("[Nyaa] Server took too long to send data.")
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         return bot.say("[Nyaa] HTTP error: " + e.message)
 
     page = etree.HTML(r.content)
-    pq = PyQuery(page)
 
     t = {}
 
-    t['name'] = pq('body > div > div:nth-child(1) > div.panel-heading > h3')
-    t['size'] = pq('body > div > div:nth-child(1) > div.panel-body > div:nth-child(4) > div:nth-child(2)')
-    t['uploader'] = pq('body > div > div:nth-child(1) > div.panel-body > div:nth-child(2) > div:nth-child(2) > a')
+    t['name'] = page.cssselect('meta[property="og:title"]')[0].get('content').replace(' :: Nyaa', '').encode('utf-8')
+    t['category'] = str(page.cssselect('meta[property="og:description"]')[0].get('content').split("|", 1)[0]).encode('utf-8')
+    t['size'] = str(page.cssselect('meta[property="og:description"]')[0].get('content').split("|", 2)[1]).encode('utf-8')
+    t['uploader'] = str(page.cssselect('meta[property="og:description"]')[0].get('content').split("|", 3)[2]).encode('utf-8')
     t['link'] = parsed_url
-    t['name'] = t['name'].text()
-    t['size'] = t['size'].text()
-    t['uploader'] = t['uploader'].text()
 
-    bot.say("[Nyaa] Name: {name} | Size: {size} | Uploaded by: {uploader} | {link}".format(**t))
+    bot.say("[Nyaa] Name: {name} | {category}| Size:{size}|{uploader} | {link}".format(**t))
 
 @module.rule('.*https?:\/\/(?:www\.)?anidex\.(?:info|moe)\/(?:torrent|dl)\/(\d+).*')
 def anidex_info(bot, trigger):
@@ -65,18 +56,15 @@ def anidex_info(bot, trigger):
         return bot.say("[Anidex] HTTP error: " + e.message)
 
     page = etree.HTML(r.content)
-    pq = PyQuery(page)
 
     t = {}
 
-    t['name'] = pq('#content > div.panel.panel-default > div > h3')
-    t['size'] = pq('#scrape_info_table > tbody > tr:nth-child(2) > td')
-    t['uploader'] = pq('#edit_torrent_form > div.row > div:nth-child(1) > table:nth-child(1) > tbody > tr:nth-child(1) > td > a')
-    t['link'] = ANIDEX_URL % trigger.group(1)
-    t['name'] = t['name'].text()
-    t['name'] = re.sub('\ \+(\d)+', '', t['name'])
-    t['size'] = t['size'].text()
-    t['uploader'] = t['uploader'].text()
+    t['name'] = page.cssselect('#content > div.panel.panel-default > div > h3')[0]
+    t['size'] = page.cssselect('#scrape_info_table > tbody > tr:nth-child(2) > td')[0]
+    t['uploader'] = page.cssselect('#edit_torrent_form > div.row > div:nth-child(1) > table:nth-child(1) > tbody > tr:nth-child(1) > td > a')[0]
+    for key in t.keys():
+        t[key] = bleach.clean(etree.tostring(t[key]), tags=[], strip=True)
+    t['link'] = parsed_url
+    t['name'] = re.sub(r'\ \+(\d)+', '', t['name'])
 
-    bot.say("[Anidex] Name: {name} | Size: {size} | Uploaded by: {uploader} | {link}".format(**t))
-
+    bot.say("[Anidex] Name:{name}| Size:{size}| Uploaded by: {uploader} | {link}".format(**t))
